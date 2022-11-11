c21c904efbbd64fd682a6bde6dd6bfb6c9a052a3
thiag <thiagooliveira1345@hotmail.com>
2022-11-10 22:09:53 -0300

    Adicionar arquivos de projeto.

---
+++ Agenda.Presentation/Models/AccountPasswordModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.Presentation.Models
+{
+    /// <summary>
+    /// Modelo de dados para a página de recuperação de senha do usuário
+    /// </summary>
+    public class AccountPasswordModel
+    {
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [Required(ErrorMessage = "Por favor, informe o seu email de acesso.")]
+        public string Email { get; set; }
+    }
+}
+
+
